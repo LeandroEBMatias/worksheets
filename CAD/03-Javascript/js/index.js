687a903d0217febb9dd5be6bd81fe71bea3274a8
Leandro Matias <leandro.b.matias@ipleiria.pt>
2021-10-14 17:50:15 +0100

    javascriptInProgress

--- CAD/03-Javascript/js/index.js
+++ CAD/03-Javascript/js/index.js
@@ -1,50 +1,86 @@
 var app = (function () {
     'use strict';
-
-    // botão kitchen light
-    var kitchenLight =
-        document.getElementById('kitchenLight');
-        
-    // icon kitchen but
-    var iconKitchenLight =
-        document.getElementById('iconKitchenLight');
-
-    // icon kitchen lampada
-    var iconKitchenBulb =
-        document.getElementById('iconKitchenBulb');
-
-    // obtem class list icon kitchen light
-    var classesIconKitchenLight =
-        iconKitchenLight.classList;
-
-    // obtem class list icon kitchen lampada
-    var classesIconKitchenBulb =
-        iconKitchenBulb.classList;
-
-
-    kitchenLight.onclick = function () {
-
-        if (classesIconKitchenLight.contains('fa-toggle-off')) {
-            classesIconKitchenLight.remove('fa-toggle-off');
-            classesIconKitchenLight.add('fa-toggle-on');
-
-            classesIconKitchenBulb.remove('fas');
-            classesIconKitchenBulb.add('far');
-
-        } else {
-            classesIconKitchenLight.remove('fa-toggle-on');
-            classesIconKitchenLight.add('fa-toggle-off');
-
-            classesIconKitchenBulb.remove('far');
-            classesIconKitchenBulb.add('fas');
-        }
-
-
-    }
+    var timeRefresh = 5
+    setTimeout(setTemp('kitchenTemp',
+        getRandomArbitrary(10, 30).toFixed(2)), timeRefresh * 6 * Math.pow(10, 4));
+    setTimeout(setTemp('livingRoomTemp',
+        getRandomArbitrary(10, 30).toFixed(2)), timeRefresh * 6 * Math.pow(10, 4));
+    change('kitchenLight', 'iconKitchenLight', 'iconKitchenBulb')
+    change('CeilingLight', 'iconCeilingLight', 'iconCeilingBulb')
+    change('AmbientLight', 'iconAmbientLight', 'iconAmbientBulb')
+    setInterval(doDate, 1000);
+    setInterval(doTime, 1000);
 
 
 })();
 
+
+function change(id_button, id_button_icon, id_bulb_icon) {
+    // botão kitchen light
+    var lightToggleButton =
+        document.getElementById(id_button);
+
+    // icon kitchen but
+    var iconToggleButton =
+        document.getElementById(id_button_icon);
+
+    // icon kitchen lampada
+    var iconBulb =
+        document.getElementById(id_bulb_icon);
+
+    // obtem class list icon kitchen light
+    var classesIconToggleButton =
+        iconToggleButton.classList;
+
+    // obtem class list icon kitchen lampada
+    var classesIconLightBulb =
+        iconBulb.classList;
+
+
+
+
+    lightToggleButton.onclick = function () {
+
+        if (classesIconToggleButton.contains('fa-toggle-off')) {
+
+            classesIconToggleButton.remove('fa-toggle-off');
+            classesIconToggleButton.add('fa-toggle-on');
+
+            classesIconLightBulb.remove('fas');
+            classesIconLightBulb.add('far');
+
+        } else {
+            classesIconToggleButton.remove('fa-toggle-on');
+            classesIconToggleButton.add('fa-toggle-off');
+
+            classesIconLightBulb.remove('far');
+            classesIconLightBulb.add('fas');
+        }
+    }
+}
+
+
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function setTemp(id_TempDiv, randomValue) {
+    document.getElementById(id_TempDiv).innerHTML = randomValue.concat(' ºC');
+}
+
+function doDate() {
+    var now = new Date();
+    var str = now.getDate() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear()
+    document.getElementById("todaysDate").innerHTML = str;
+}
+
+function doTime() {
+    var now = new Date();
+    var str = now.getHours() + ":" + (now.getMinutes()) + ":" + now.getSeconds();
+    document.getElementById("nowTime").innerHTML = str;
+}
+now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
 
 // console.log(app.a);
 // console.log(app.a)
